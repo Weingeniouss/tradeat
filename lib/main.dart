b8bc5a732f4b4d93d265dda1572711c1339d6ae8
Srimantrao <srimantraosonar@gmail.com>
2025-05-27 16:24:25 +0530

    tradeAT

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class MyApp extends StatelessWidget {
         ),
         theme: ThemeData(fontFamily: 'Poppins',dialogBackgroundColor: AppColor.textfield_color),
         debugShowCheckedModeBanner: false,
-        home: AuthMobileNumber(),
+        home: Logo(),
       ),
     );
   }
